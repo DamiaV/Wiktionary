036ddacc6e46f4397c9b6048f6bd84906395f036
Darmo117 <darmo117@gmail.com>
2025-02-22 10:27:11 +0100

    continue implementing Gadget-wikt.manage-sections.js (WIP)

--- JS-CSS/Mediawiki/Gadget-wikt.manage-sections.js
+++ JS-CSS/Mediawiki/Gadget-wikt.manage-sections.js
@@ -4,28 +4,79 @@
   console.log("Chargement de Gadget-wikt.manage-sections.js…");
 
   /**
-   * @param $sectionTitle {jQuery}
+   * @param $sectionTitle
    * @return {number}
    */
-  function getSectionLine($sectionTitle) {
-    const title = $sectionTitle.find("span:first-child").text();
-    // TODO
+  function getSectionIndexInHtml($sectionTitle) {
+    let count = 0;
+    for (const node of $(".mw-parser-output").children()) {
+      if ($(node).classList.contains("mw-heading")) count++;
+      if (node === $sectionTitle[0]) return count;
+    }
+    return -1;
   }
 
   /**
-   * @param $sectionTitle {jQuery}
+   * @param wikicode {string}
+   * @param htmlIndex {number}
+   * @return {number}
+   */
+  function getSectionIndexInWikicode(wikicode, htmlIndex) {
+    let i = 0;
+    for (const line of wikicode.split("\n")) {
+      if (htmlIndex === 0) return i;
+      if (/(={2,6}) *[^=]+ *\1/.test(line)) htmlIndex--;
+      i++;
+    }
+    return htmlIndex === 0 ? i : -1;
+  }
+
+  /**
+   * @param $sectionTitle
    * @param up {boolean}
    */
   function moveSection($sectionTitle, up) {
-    // TODO
+    $.get(
+        location.href,
+        {action: "raw"},
+        callback
+    )
+
+    /**
+     * @param wikicode {string}
+     */
+    function callback(wikicode) {
+      const htmlI = getSectionIndexInHtml($sectionTitle);
+      const wikicodeI = getSectionIndexInWikicode(wikicode, htmlI);
+      // TODO
+    }
   }
 
   /**
-   * @param $sectionTitle {jQuery}
+   * @param $sectionTitle
    * @param level {number}
    */
   function setSectionLevel($sectionTitle, level) {
     if (level < 2 || level > 6) throw new Error("level must be between 2 and 6");
-    // TODO
+    $.get(
+        location.href,
+        {action: "raw"},
+        callback
+    )
+
+    /**
+     * @param wikicode {string}
+     */
+    function callback(wikicode) {
+      const htmlI = getSectionIndexInHtml($sectionTitle);
+      const wikicodeI = getSectionIndexInWikicode(wikicode, htmlI);
+      const tagName = $sectionTitle.children()[0].tagName;
+      const currentLevel = tagName.charAt(tagName.length - 1);
+
+      $sectionTitle.classList.remove("mw-heading" + currentLevel);
+      $sectionTitle.classList.add("mw-heading" + level);
+      // TODO replace current H* tag with new one
+      // TODO update wikicode
+    }
   }
 })();
