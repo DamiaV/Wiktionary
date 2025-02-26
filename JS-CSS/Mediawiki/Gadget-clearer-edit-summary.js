42a030e7672f1258b5cb547ed85bc799e771198a
Darmo117 <darmo117@gmail.com>
2025-02-26 15:35:11 +0100

    fix: remove breadcrumb in Gadget-clearer-edit-summary.js as it would generate invalid links

--- JS-CSS/Mediawiki/Gadget-clearer-edit-summary.js
+++ JS-CSS/Mediawiki/Gadget-clearer-edit-summary.js
@@ -3,32 +3,14 @@
  * en listant le fil d’Ariane de la section modifiée.
  */
 $(function () {
+  console.log("Chargement de Gadget-clearer-edit-summary.js…");
   // Add `&summary=/* <summary> */` to each “Edit” link.
-  if (mw.config.get("wgAction") === "view") {
-    console.log("Chargement de Gadget-clearer-edit-summary.js…");
-    $(".mw-editsection").each(function () {
-      var $this = $(this);
-      var $title = $this.prev(); // H* tag that precedes the current element
-      if (!$title.length)
-        return;
-      // Build breadcrumb
-      var titleLevel = +$title.prop("tagName").charAt(1);
-      var titles = [];
-      for (var level = titleLevel; level > 1; level--) {
-        titles.unshift($title.attr("id").replace(/_/g, "%20"));
-        if (level > 2) { // Look for the containing section header
-          $title = $title.parent()
-              .prevAll(".mw-heading" + (level - 1)) // All previous headers of directly higher level
-              .first() // Closest previous header of directly higher level
-              .children() // H* tags
-              .first(); // First (and only) H* tag
-          if (!$title.length) break;
-        }
-      }
-      // When "Show me both editor tabs" enabled, there will be two links: visual and wikitext.
-      $this.find("a").attr("href", function (_, href) {
-        return href + "&summary=/*%20" + titles.join("/") + "%20*/%20";
-      });
-    });
-  }
+  $(".mw-editsection").each(function () {
+    const $this = $(this);
+    let $title = $this.prev(); // H* tag that precedes the current element
+    if (!$title.length) return;
+    // When "Show me both editor tabs" enabled, there will be two links: visual and wikitext.
+    $this.find("a").attr("href", (_, href) =>
+        href + "&summary=/*%20" + $title.attr("id").replaceAll("_", " ") + "%20*/%20");
+  });
 });
