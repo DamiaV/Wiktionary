5955e27047042e3c96c3ca027dc1b05db84b9736
Darmo117 <darmo117@gmail.com>
2025-03-29 16:11:21 +0100

    treatment of [[Wiktionnaire:Bots/Requêtes#Clés de tri en same du Nord]]

--- fr-wiktionary/bot/replacer.py
+++ fr-wiktionary/bot/replacer.py
@@ -11,21 +11,31 @@ MODE_EMBEDS = 'embeds'
 MODE_LINKS = 'links'
 
 # Arguments #
-PAGE_NAME = 'Catégorie:Appels de modèles incorrects:S'
-SUMMARY = 'Remplacement du paramètre id par num dans {{S}}'
+PAGE_NAME = 'Catégorie:same du Nord'
+SUMMARY = 'Traitement de [[Wiktionnaire:Bots/Requêtes#Clés de tri en same du Nord]]'
 LINK_MODE = MODE_CAT
 # language=pythonregexp
-NEEDLE = r'\{\{S\|([^}]+?)\|(?:id|ID)=([^|}]+)(\|[^}]+)*}}'
-REPL = r'{{S|\1|num=\2\3}}'
+NEEDLE = r'\{\{clé de tri\|[^|}]+}}'
+REPL = '{{clé de tri||se}}'
+
+
+def CONDITION(text: str) -> bool:
+    if '{{clé de tri' not in text:
+        return False
+    matches = re.findall(r'== *\{\{langue\|([^}]+)}} *==', text)
+    return len(matches) == 1 and matches[0] == 'se'
 
 
 def handle_page(page: pwb.Page) -> None:
     print(page.title())
     old_text = page.text
+    if not CONDITION(old_text):
+        print('Condition is false, skipped.')
+        return
     page.text = re.sub(NEEDLE, REPL, old_text.strip())
     if old_text != page.text:
         try:
-            page.save(summary=SUMMARY, quiet=True)
+            page.save(summary=SUMMARY, quiet=True, minor=False)
             print('Done.')
         except pwb.exceptions.PageSaveRelatedError as e:
             print('Protected page, skipped:', e)
