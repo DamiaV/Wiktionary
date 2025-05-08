99f4c4b503c55161c5b6154fa6d9059fdbc11113
Darmo117 <darmo117@gmail.com>
2025-05-08 17:54:52 +0200

    import Module:langues/analyse

---
+++ fr-wiktionary/models-modules/langues/analyse.lua
@@ -0,0 +1,71 @@
+local m_bases = require("Module:bases")
+
+local languagesData = mw.loadData("Module:langues/data")
+
+local p = {}
+
+local function getSortKey(langName)
+  if mw.ustring.toNFD(langName) == langName and not mw.ustring.find(langName, "[’)(]") then
+    return nil
+  end
+  local sortKey = mw.ustring.toNFD(langName)
+  sortKey = mw.ustring.gsub(sortKey, "[^%a-]", "")
+  sortKey = mw.ustring.gsub(sortKey, "æ", "ae")
+  sortKey = mw.ustring.gsub(sortKey, "œ", "oe")
+  sortKey = mw.ustring.gsub(sortKey, "['’)(]", "")
+  sortKey = mw.ustring.gsub(sortKey, "[-\\/]", " ")
+  return sortKey
+end
+
+local function sortLanguages(code1, code2)
+  local t1 = languagesData[code1]["tri"] and languagesData[code1]["tri"] or
+      getSortKey(languagesData[code1]["nom"]) or languagesData[code1]["nom"]
+  local t2 = languagesData[code2]["tri"] and languagesData[code2]["tri"] or
+      getSortKey(languagesData[code2]["nom"]) or languagesData[code2]["nom"]
+  return t1 < t2
+end
+
+function p.showLanguagesTable(_)
+  local titres = { "n", "Code", "Nom", "Lien Wikimédia" }
+
+  local keyset = {}
+  local n = 0
+
+  for k, _ in pairs(languagesData) do
+    n = n + 1
+    keyset[n] = k
+  end
+
+  local result = {
+    "Cette liste est générée automatiquement à partir de [[Module:langues/data]].\n\n" ..
+        "Il y a actuellement " .. tostring(n) .. " codes de langue dans cette liste.\n" ..
+        "La liste peut être triée par nom de langue (défaut) ou par code."
+  }
+
+  table.sort(keyset, sortLanguages)
+
+  table.insert(result, m_bases.tableau_entete(titres))
+  for i, code in ipairs(keyset) do
+    local infos = languagesData[code]
+    local langName = infos["nom"] or "NOM MANQUANT"
+    local langSortKey = infos["tri"] or getSortKey(langName) or langName
+    local langCategory = m_bases.fait_categorie(langName, langName, true)
+    local langColumn = 'data-sort-value="' .. langSortKey .. '"|' .. langCategory
+    local wmLink = infos["wmlien"] or ""
+    local row = { i, '<span id="' .. code .. '">' .. code .. '</span>', langColumn, wmLink }
+    table.insert(result, m_bases.tableau_ligne(row))
+  end
+
+  table.insert(result, m_bases.tableau_fin())
+  return table.concat(result, "\n")
+end
+
+function p.showLanguagesPython(_)
+  local lines = {}
+  for k, v in pairs(languagesData) do
+    table.insert(lines, '    "' .. k .. '": "' .. v["nom"] .. '",')
+  end
+  return "<pre>\nlanguages = {\n" .. table.concat(lines, "\n") .. "\n}\n</pre>\n"
+end
+
+return p
