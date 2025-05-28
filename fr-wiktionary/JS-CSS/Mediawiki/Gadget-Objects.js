35057f6f17908336f681b28b22291ec4828132bd
Darmo117 <darmo117@gmail.com>
2025-05-28 13:10:15 +0200

    refactor: remove obsolete gadgets

--- fr-wiktionary/JS-CSS/Mediawiki/Gadget-Objects.js
+++
@@ -1,3 +0,0 @@
-/*[[Catégorie:JavaScript du Wiktionnaire|Objects.js]]*/
-/* Importe la même page de Wikipédia */
-mw.loader.load('//fr.wikipedia.org/w/index.php?title=MediaWiki:Gadget-Objects.js&action=raw&ctype=text/javascript&dontcountme=s');
--- fr-wiktionary/JS-CSS/Mediawiki/Gadget-disableTOC.js
+++
@@ -1,4 +0,0 @@
-//[[Catégorie:JavaScript du Wiktionnaire|disableTOC.js]]
-/* Rétablit le style les sommaires (TOC : table of contents) du main.css initial (donc annule celui de [[Mediawiki:Common.css]]) */
-
-var disableTOC = true;
