d2feb2e4e573b34372cd7d6fc78b1eeccf5a49bc
Darmo117 <darmo117@gmail.com>
2025-05-28 02:01:56 +0200

    refactor: Gadget-CategoryAboveAll.js

--- fr-wiktionary/JS-CSS/Mediawiki/Gadget-CategoryAboveAll.js
+++ fr-wiktionary/JS-CSS/Mediawiki/Gadget-CategoryAboveAll.js
@@ -1,11 +1,12 @@
 // [[Catégorie:JavaScript du Wiktionnaire|CategoryAboveAll.js]]
 //* by [[Commons:User:Ianezz|Ianezz]] ([[Commons:Commons:Village_pump/Archive/2009Sep#CSS_placement_of_categories]]), based on [[:wikinews:Help:User_style#Moving_categories_to_top]]
+"use strict";
 
 function catsattop() {
-  if (mw.config.get('wgCanonicalNamespace') == "Special") return;
-  var cats = document.getElementById('catlinks');
-  var bc = document.getElementById('bodyContent');
-  bc.insertBefore(cats, bc.childNodes[0]);
+  if (mw.config.get("wgCanonicalNamespace") === "Special") return;
+  const catLinks = document.getElementById("catlinks");
+  const bodyContent = document.getElementById("bodyContent");
+  bodyContent.insertBefore(catLinks, bodyContent.childNodes[0]);
 }
 
 $(catsattop);
