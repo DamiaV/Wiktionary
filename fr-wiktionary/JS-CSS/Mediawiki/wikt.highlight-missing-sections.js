2ba88dcc6a6b93192cbde77b22d8acd3cc530f19
Darmo117 <darmo117@gmail.com>
2025-05-30 19:51:48 +0200

    refactor: flatten structure

--- fr-wiktionary/JS-CSS/Mediawiki/wikt.highlight-missing-sections.js
+++ fr-wiktionary/JS-CSS/Mediawiki/wikt.highlight-missing-sections.js
@@ -9,10 +9,11 @@
  * correspondant à un code de langue mais dont la page pointée ne
  * contient pas de section pour le code en question.
  **********************************************************************
- * v1.0 2020-09-18 Initial version
- * v1.1 2021-10-17 Gadget now goes through redirections
- * v1.2 2025-05-23 Refactor; use new [[MediaWiki:Gadget-wikt.core.languages.json]]
+ * v1.0 2020-09-18 Initial version.
+ * v1.1 2021-10-17 Gadget now goes through redirections.
+ * v1.2 2025-05-23 Refactor; use new [[MediaWiki:Gadget-wikt.core.languages.json]].
  * v1.3 2025-05-26 Conversion into a module.
+ * v1.4 2025-05-30 Refactor: flatten structure.
  **********************************************************************
  * [[Catégorie:JavaScript du Wiktionnaire|highlight missing sections]]
  */
@@ -23,106 +24,87 @@ const { getLanguages } = require("./wikt.core.languages.js");
 
 console.log("Chargement de Gadget-wikt.highlight-missing-sections.js…");
 
-const highlightMissingSections = {
-  NAME: "Highlight Missing Sections",
+const VERSION = "1.4";
+const api = new mw.Api({ userAgent: "Gadget-wikt.highlight-missing-sections/" + VERSION });
+const languages = getLanguages(true);
 
-  VERSION: "1.3",
+/**
+ * Retrieve the content of the given page, following redirections.
+ * Does not check for redirection loops.
+ * @param pageTitle {string} Page’s title.
+ * @param callback {function} Function to call at the end of the asynchronous call.
+ * Takes in the content of the page.
+ */
+function getPageContent(pageTitle, callback) {
+  api.get({
+    action: "query",
+    prop: "revisions",
+    titles: pageTitle,
+    rvslots: "main",
+    rvprop: "content",
+  }).then((queryResult) => {
+    let page;
+    // Get first (and only) entry
+    // noinspection LoopStatementThatDoesntLoopJS
+    for (page of Object.values(queryResult.query.pages))
+      break;
+    const content = page["revisions"][0]["slots"]["main"]["*"];
+    const match = /^#REDIRECT\[\[([^\[]+)]]$/.exec(content.trim());
+    if (match) getPageContent(match[1], callback);
+    else callback(content);
+  });
+}
 
-  init: function () {
-    this.api = new mw.Api({ userAgent: "Gadget-wikt.highlight-missing-sections/" + highlightMissingSections.VERSION });
-    const languagesData = getLanguages(true);
-    this._onResponse(Array.from(languagesData.keys()));
-  },
+/**
+ * Highlights the given link if the target page doesn’t
+ * feature a section for the given language code.
+ * @param $link {Object} The jQuery link object.
+ * @param langCode {string} The language code.
+ * @param pageCode {string} The code of the page the link points to.
+ */
+function highlightLink($link, langCode, pageCode) {
+  if (!pageCode.includes(`{{langue|${langCode}}}`)) {
+    $link.addClass(["wikt-missing-entry", `wikt-missing-entry-${langCode}`]);
+    $link.attr("title", $link.attr("title") + ` (section « ${langCode} » manquante)`);
+  }
+}
 
-  /**
-   * Callback function called when the GET query succeeds in init().
-   * @param languages {string[]} The array containing language codes.
-   * @private
-   */
-  _onResponse: function (languages) {
-    const links = [];
-    const serverName = mw.config.get("wgServerName");
-    const namespaces = mw.config.get("wgNamespaceIds");
+const links = [];
+const serverName = mw.config.get("wgServerName");
+const namespaces = mw.config.get("wgNamespaceIds");
 
-    $(".mw-parser-output a:not(.new)").each((_, link) => {
-      const $link = $(link);
-      const href = $link.prop("href");
-      if (!href) return;
-      const url = new URL(href);
-      const hash = decodeURIComponent(url.hash.substring(1)); // Strip #
+$(".mw-parser-output a:not(.new)").each((_, link) => {
+  const $link = $(link);
+  const href = $link.prop("href");
+  if (!href) return;
+  const url = new URL(href);
+  const hash = decodeURIComponent(url.hash.substring(1)); // Strip #
 
-      if (url.hostname === serverName &&
-          url.pathname.startsWith("/wiki/") &&
-          languages.includes(hash)) {
-        const pageTitle = decodeURIComponent(url.pathname.substring(6));
-        const namespace = pageTitle.substring(0, pageTitle.indexOf(":")).toLowerCase();
+  if (url.hostname === serverName &&
+      url.pathname.startsWith("/wiki/") &&
+      languages.has(hash)) {
+    const pageTitle = decodeURIComponent(url.pathname.substring(6));
+    const namespace = pageTitle.substring(0, pageTitle.indexOf(":")).toLowerCase();
 
-        // Check that the link points to the main namespace, and that the namespace is not a language code.
-        if (!namespaces[namespace] && !languages.includes(namespace)) {
-          links.push({
-            $link: $link,
-            pageTitle: pageTitle,
-            langCode: hash,
-          });
-        }
-      }
-    });
-
-    // Cache pages’ code in case same page is linked several times
-    const pageCodes = {};
-    links.forEach((item) => {
-      if (!pageCodes[item.pageTitle])
-        this._getPageContent(item.pageTitle, content => {
-          pageCodes[item.pageTitle] = content;
-          this._highlightLink(item.$link, item.langCode, pageCodes[item.pageTitle]);
-        });
-      else this._highlightLink(item.$link, item.langCode, pageCodes[item.pageTitle]);
-    });
-  },
-
-  /**
-   * Retrieve the content of the given page, following redirections.
-   * Does not check for redirection loops.
-   * @param pageTitle {string} Page’s title.
-   * @param callback {function} Function to call at the end of the asynchronous call.
-   * Takes in the content of the page.
-   * @private
-   */
-  _getPageContent: function (pageTitle, callback) {
-    this.api.get({
-      action: "query",
-      prop: "revisions",
-      titles: pageTitle,
-      rvslots: "main",
-      rvprop: "content",
-    }).then((queryResult) => {
-      let pageID;
-      for (pageID in queryResult.query.pages)
-        if (queryResult.query.pages.hasOwnProperty(pageID))
-          break;
-      const revisions = queryResult.query.pages[pageID]["revisions"];
-      const content = revisions[0]["slots"]["main"]["*"];
-      const match = /^#REDIRECT\[\[([^\[]+)]]$/.exec(content.trim());
-      if (match) this._getPageContent(match[1], callback);
-      else callback(content);
-    });
-  },
-
-  /**
-   * Highlights the given link if the target page doesn’t
-   * feature a section for the given language code.
-   * @param $link {Object} The jQuery link object.
-   * @param langCode {string} The language code.
-   * @param pageCode {string} The code of the page the link points to.
-   * @private
-   */
-  _highlightLink: function ($link, langCode, pageCode) {
-    if (!pageCode.includes(`{{langue|${langCode}}}`)) {
-      $link.addClass(["wikt-missing-entry", `wikt-missing-entry-${langCode}`]);
-      $link.attr("title", $link.attr("title") + ` (section « ${langCode} » manquante)`);
+    // Check that the link points to the main namespace, and that the namespace is not a language code.
+    if (!namespaces[namespace] && !languages.has(namespace)) {
+      links.push({
+        $link: $link,
+        pageTitle: pageTitle,
+        langCode: hash,
+      });
     }
-  },
-};
+  }
+});
 
-highlightMissingSections.init();
+// Cache pages’ code in case same page is linked several times
+const pageCodes = {};
+links.forEach((item) => {
+  if (!pageCodes[item.pageTitle])
+    getPageContent(item.pageTitle, content => {
+      pageCodes[item.pageTitle] = content;
+      highlightLink(item.$link, item.langCode, pageCodes[item.pageTitle]);
+    });
+  else highlightLink(item.$link, item.langCode, pageCodes[item.pageTitle]);
+});
 // </nowiki>
