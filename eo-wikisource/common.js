b7feef1ba88f63104ce12830d3e0d254f8e5b0c1
Darmo117 <darmo117@gmail.com>
2025-06-08 17:19:03 +0200

    refactor

--- eo-wikisource/common.js
+++ eo-wikisource/common.js
@@ -1,20 +1,31 @@
-mw.loader.load("https://fr.wikisource.org/wiki/Utilisateur:Lepticed7/Sliders.js?action=raw&ctype=text/javascript");
+"use strict";
 
+//mw.loader.load("https://fr.wikisource.org/wiki/Utilisateur:Lepticed7/Sliders.js?action=raw&ctype=text/javascript");
+
+/*
+ * Redirect Diosiero: pages to the corresponding File: page on Commons
+ */
 const filePageRegex = /^\/wiki\/Dosiero:(.+)$/;
 const m = filePageRegex.exec(location.pathname);
 if (m) location.replace(`https://commons.wikimedia.org/wiki/File:${m[1]}`);
 
-document.querySelectorAll("a").forEach((link) => {
+/**
+ * @param link {HTMLAnchorElement}
+ */
+function replaceLink(link) {
   const m = filePageRegex.exec(link.pathname);
   if (m) link.href = `https://commons.wikimedia.org/wiki/File:${m[1]}`;
+}
+
+document.querySelectorAll("a").forEach((link) => {
+  replaceLink(link);
 });
 
 const obs = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     const target = mutation.target;
     if (!(target instanceof HTMLAnchorElement)) return;
-    const m = filePageRegex.exec(target.pathname);
-    if (m) target.href = `https://commons.wikimedia.org/wiki/File:${m[1]}`;
+    replaceLink(target);
   });
 });
 obs.observe(document.body, { childList: true, subtree: true });
