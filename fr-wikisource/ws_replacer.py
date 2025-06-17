c9808761249c098017f99e43f3cbb44652b74223
Darmo117 <darmo117@gmail.com>
2025-06-17 22:03:21 +0200

    refactor

--- fr-wikisource/ws_replacer.py
+++ fr-wikisource/ws_replacer.py
@@ -1,30 +1,24 @@
+import re
+
 import pywikibot as pwb
 
-site = pwb.Site()
 
-for i in range(1, 87 + 1):
-    page = pwb.Page(site, title=f'Page:Poisson - Alphabet nouveau - 1609.djvu/{i}')
-    prev = page.text
-    page.text = page.text.replace('[ch]', '{{X|ch}}')
+def main() -> None:
+    site = pwb.Site()
 
-    # new_text = ''
-    # for line in page.text.split('\n'):
-    #     if len(line) > 0 and line[0] == '|':
-    #         try:
-    #             i = line.index(" ||")
-    #             line = line[:i].replace('ch', '[ch]') + line[i:]
-    #         except ValueError:
-    #             pass
-    #     else:
-    #         line = line.replace('ch', '[ch]')
-    #     new_text += line + '\n'
-    # page.text = new_text.strip() + '\n'
+    for i in range(1, 75 + 1):
+        page = pwb.Page(site, title=f'Page:Marot, Jean - Le Recueil Jehan Marot.djvu/{i}')
+        prev = page.text
+        page.text = re.sub(r'{{em\|1}}', '{{em}}', page.text)
 
-    if prev != page.text:
-        page.save(
-            summary='(Modification par bot) Remplacement [ch] par {{X|ch}} '
-                    '(cf. [[Wikisource:Scriptorium/Juillet 2020#Caractère_difficile_à_transcrire]])',
-            minor=True
-        )
-    else:
-        print(f'Page [[{page.title()}]] not changed')
+        if prev != page.text:
+            page.save(
+                summary='(Modification par bot) Simplification de {{em|1}}',
+                minor=True
+            )
+        else:
+            print(f'Page [[{page.title()}]] not changed')
+
+
+if __name__ == '__main__':
+    main()
