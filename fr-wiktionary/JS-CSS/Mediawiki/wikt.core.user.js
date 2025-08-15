9239cbbd874644e6a38a85d486238b88ce6dbf79
damia <damia.vergnet@protonmail.com>
2025-08-15 13:41:34 +0200

    Add function to get user’s gender

--- fr-wiktionary/JS-CSS/Mediawiki/wikt.core.user.js
+++ fr-wiktionary/JS-CSS/Mediawiki/wikt.core.user.js
@@ -7,8 +7,28 @@
  * @return {boolean} True if the user belongs to the group.
  */
 function isInUsergroup(groupname) {
-  /** @type {string[]} */
   return mw.config.get("wgUserGroups").includes(groupname);
+}
+
+/**
+ * Get the gender of the given user. This function makes a call to the MediaWiki API.
+ * @param {mw.Api} api The MediaWiki API instance to use.
+ * @param {string?} username The user’s username. If null, the current user’s username will be used.
+ * @return {JQuery.Promise<"female" | "male" | "unknown">} A Promise object returning the gender as a string.
+ * The string "unknown" will also be returned if the username is invalid.
+ */
+async function getGender(api, username) {
+  const params = {
+    action: "query",
+    list: "users",
+    ususers: username || mw.config.get("wgUserName"),
+    usprop: "gender",
+    format: "json"
+  };
+
+  return api.get(params).then((data) => {
+    return data.query.users[0].gender || "unknown";
+  });
 }
 
 // </nowiki>
@@ -18,4 +38,5 @@ function isInUsergroup(groupname) {
  */
 module.exports = {
   isInUsergroup,
+  getGender,
 };
