2c11f2432004fad8ffa2db0663b2922c030de323
damia <damia.vergnet@protonmail.com>
2025-10-22 19:15:59 +0200

    Fix wrong language codes returned by p.getLanguageCode()

--- fr-wiktionary/models-modules/langues.lua
+++ fr-wiktionary/models-modules/langues.lua
@@ -21,8 +21,8 @@ p.specialCodes = {
 
 --- Return the name of the language matching the given language code.
 --- @param code string A language code.
---- @param allowSpecial boolean If true, codes marked as group or special also will be considered.
---- @return string|nil The matching language name, nil otherwise.
+--- @param allowSpecial? boolean If true, codes marked as group or special also will be considered.
+--- @return string|nil # The matching language name, nil otherwise.
 function p.getName(code, allowSpecial)
   if not code or not languagesData[code] or not allowSpecial and (languagesData[code].isSpecial or languagesData[code].isGroup) then
     return nil
@@ -32,8 +32,8 @@ end
 
 --- Return the sort key for the given language code.
 --- @param code string A language code.
---- @param allowSpecial boolean If true, codes marked as group or special will also be considered.
---- @return string|nil The sort key for the language, nil if the code is invalid.
+--- @param allowSpecial? boolean If true, codes marked as group or special will also be considered.
+--- @return string|nil # The sort key for the language, nil if the code is invalid.
 function p.getSortKey(code, allowSpecial)
   if not code or not languagesData[code] or not allowSpecial and (languagesData[code].isSpecial or languagesData[code].isGroup) then
     return nil
@@ -45,7 +45,7 @@ end
 --- @param frame frame
 --- Parameters:
 ---  parent.args[1] (string): Language code.
---- @return string The name of the language or an empty string if the code is invalid.
+--- @return string # The name of the language or an empty string if the code is invalid.
 function p.languageName(frame)
   local args = m_params.process(frame:getParent().args, {
     [1] = {},
@@ -57,7 +57,7 @@ end
 --- @param frame frame
 --- Parameters:
 ---  args[1] (string, optional): Language code.
---- @return string|nil The sort key for the language, nil if the code is invalid.
+--- @return string|nil # The sort key for the language, nil if the code is invalid.
 function p.languageSortKey(frame)
   local args = m_params.process(frame:getParent().args, {
     [1] = {},
@@ -69,8 +69,8 @@ end
 --- This function is used by the template {{L}}.
 --- @param frame frame
 --- Parameters:
----  parent.args[1] (string): A language code.
---- @return string The capitalized name of the language, an error message if none matched.
+---  parent.args[1] (string, optional): A language code.
+--- @return string # The capitalized name of the language, an error message if none matched.
 function p.languageNameForList(frame)
   local args = m_params.process(frame:getParent().args, {
     [1] = {}
@@ -92,7 +92,7 @@ end
 
 --- Return the Wikimedia language code for the given internal language code if it exists.
 --- @param code string A language code.
---- @return string The corresponding Wikimedia language code, or nil if none matched.
+--- @return string|nil # The corresponding Wikimedia language code, or nil if none matched.
 function p.getWikimediaCode(code)
   if not code or not languagesData[code] then
     return nil
@@ -104,7 +104,7 @@ end
 --- @param frame frame
 --- Parameters:
 ---  parent.args[1] (string, optional): A language code.
---- @return string The corresponding Wikimedia language code, or an empty string if none matched.
+--- @return string # The corresponding Wikimedia language code, or an empty string if none matched.
 function p.wikimediaCode(frame)
   local args = m_params.process(frame:getParent().args, {
     [1] = {},
@@ -115,50 +115,29 @@ end
 
 --- Check whether a page in the “Portail” namespace exists for the given language code.
 --- @param code string A language code.
---- @return boolean True if a “Portail” page exists, false otherwise.
+--- @return boolean # True if a “Portail” page exists, false otherwise.
 function p.hasPortal(code)
   return languagesData[code] ~= nil and languagesData[code].hasPortal
 end
 
 --- Check whether a Wiktionary exists for the given language code.
 --- @param code string Le code de langue.
---- @return boolean True if a Wiktionary exists, false otherwise.
+--- @return boolean # True if a Wiktionary exists, false otherwise.
 function p.hasWiktionary(code)
   return languagesData[code] ~= nil and languagesData[code].wiktionaryExists
 end
 
 --- Return the code corresponding to the given language name.
---- If there are more than one, keep the shortest one.
---- The function also takes code aliases into account (e.g. "anglo-saxon" for "vieil anglais").
---- Special case: if there exists a code that strictly equals a language name,
---- it will be returned even if a shorter code exists (e.g. "vieil écossais" vs "vieux scots"),
---- unless there exists a code with 3 characters or less (e.g. "créole guadeloupéen" vs "gcf"),
---- except if the language name is "normand".
 --- @param languageName string A language name.
---- @param allowSpecial boolean If true, codes marked as group or special also will be considered.
---- @return string|nil The language’s code, or nil if none matched.
+--- @param allowSpecial? boolean If true, codes marked as group or special also will be considered.
+--- @return string | nil # The language’s code, or nil if none matched.
 function p.getLanguageCode(languageName, allowSpecial)
-  if languageName == "normand" then
-    -- Special case: we prefer to return "normand" instead of shorter code "nrf"
-    return languageName
-  end
-  local result
   for code, languageData in pairs(languagesData) do
-    if languageName == languageData.name and
-        (allowSpecial or not languageData.isGroup and not languageData.isSpecial) then
-      local codeLength = mw.ustring.len(code)
-      if result == nil or code == languageName or
-          codeLength < mw.ustring.len(result) and (result ~= languageName or codeLength <= 3)
-      then
-        result = code
-      end
+    if languageName == languageData.name and (allowSpecial or (not languageData.isGroup and not languageData.isSpecial)) then
+      return languageData.aliasOf or code
     end
   end
-  -- If no match yet, consider the name as an alias and try again
-  if result == nil and languagesData[languageName] and languagesData[languageName].name then
-    return p.getLanguageCode(languagesData[languageName].name)
-  end
-  return result
+  return nil
 end
 
 --- Return the code corresponding to the given language name.
@@ -172,7 +151,7 @@ end
 --- Parameters:
 ---  parent.args[1] (string, optional): A language name. Defaults to the current page’s title.
 ---  parent.args["codes spéciaux"] (boolean, optional): If true, codes marked as group or special also will be considered.
---- @return string The language’s code, or an empty string if none matched.
+--- @return string # The language’s code, or an empty string if none matched.
 function p.languageCode(frame)
   local args = m_params.process(frame:getParent().args, {
     [1] = { default = mw.title.getCurrentTitle().text },
